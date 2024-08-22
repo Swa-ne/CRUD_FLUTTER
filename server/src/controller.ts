03ee4e4345ccbab11ed7f85b1358ec69957e05f6
swa-ne <spaulbautista@gmail.com>
2024-08-22 23:59:38 +0800

    Updated start script path to src/index.ts to match project structure changes.

--- server/src/controller.ts
+++ server/src/controller.ts
@@ -5,7 +5,6 @@ import { addStudent, getStudent, getStudents, removeStudent, updateStudent } fro
 export const getStudentsController = async (req: Request, res: Response) => {
     try {
         const students = await getStudents()
-        console.log("hiiiiiiiiiii")
         return res.status(200).json({ 'message': students });
     } catch (error) {
         return res.status(500).json({ 'error': 'Internal Server Error' });
